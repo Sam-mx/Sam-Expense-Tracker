c7047220981cdffc5e4aa4cf55ac3a84636862f9
San Shwe <sanxhwe@gmail.com>
2024-03-29 22:36:29 +0630

    Details Component

--- src/components/Details/Details.jsx
+++ src/components/Details/Details.jsx
@@ -1,8 +1,24 @@
 import React from 'react'
+import{Card, CardHeader,CardContent, Typography} from '@material-ui/core'
+import { Doughnut } from 'react-chartjs-2'
 
-const Details = () => {
+import useTransactions from '../../useTransactions'
+
+import useStyles from './styles'
+
+
+const Details = ({title}) => {
+    const classes = useStyles();
+    const {total, chartData} = useTransactions(title);
+
   return (
-    <div>Details</div>
+    <Card className={title==='Income'? classes.income : classes.expense}>
+        <CardHeader title={title}/>
+        <CardContent>
+            <Typography variant="h5">Rs {total}</Typography>
+            <Doughnut data={chartData}/>
+        </CardContent>
+    </Card>
   )
 }
 
